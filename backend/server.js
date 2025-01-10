8781c1d799042697c4b074733d9d44f4e5c916be
Keval <kevalmehta885@gmail.com>
2025-01-10 18:03:15 +0530

    Routes are created

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import matchRoutes from "./routes/match.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -9,10 +13,10 @@ const PORT = process.env.PORT || 5500;
 
 app.use(express.json());
 
-app.use("/api/auth");
-app.use("/api/users");
-app.use("/api/matches");
-app.use("/api/messages");
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/matches", matchRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:5500`);
