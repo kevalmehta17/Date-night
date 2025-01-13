1ba8adeeed9d132ef16538bf9b86b04af7cb838c
Keval <kevalmehta885@gmail.com>
2025-01-13 11:38:58 +0530

    protectRoute endpoint builded

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,10 +2,22 @@ import express from "express";
 
 import { signup, login, logout } from "../controllers/auth.controller.js";
 
+import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/signup", signup);
+
 router.post("/login", login);
+
 router.post("/logout", logout);
+
+//this route will ensure that only authenticated users can access it
+router.get("/me", protectRoute, (req, res) => {
+  res.json({
+    success: true,
+    user: req.user,
+  });
+});
 
 export default router;
