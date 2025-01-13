c35ddec189c64c6a8c22f4039f37a3e1c6b6f56a
Keval <kevalmehta885@gmail.com>
2025-01-13 17:41:47 +0530

    Modified the code

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,12 +1,13 @@
-import { vs as cloudinary } from "cloudinary";
+import cloudinary from "cloudinary";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-cloudinary.config({
+// Set up Cloudinary config using v2
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+export default cloudinary.v2; // Export the v2 instance
