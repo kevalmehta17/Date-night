e7bbc10e29dba86897fea3e2386c83b9298b69ee
Keval <kevalmehta885@gmail.com>
2025-01-13 17:51:10 +0530

    message route and controller setup

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware";
 
 const router = express.Router();
+
+router.use(protectRoute); // this middleware first runs the protectRoute function then the function below
+// ex:- router.post("/send",protectRoute ,sendMessage);
+
+router.post("/send", sendMessage);
+router.get("/conversation/:userId", getConversation);
 
 export default router;
