82ca68f979d057b0964320b1f730b1477058257b
Keval <kevalmehta885@gmail.com>
2025-01-13 15:52:53 +0530

    match route setup

--- backend/routes/match.route.js
+++ backend/routes/match.route.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
+
+router.post("/swipe-right/:likeUserId", protectRoute, swipeRight);
+router.post("/swipe-left/:dislikeUserId", protectRoute, swipeLeft);
+
+router.get("/matches", protectRoute, getMatches);
+router.get("/user-profiles", protectRoute, getUserProfiles);
 
 export default router;
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { updateProfile } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.put("/update", protectRoute, updateRoute);
+router.put("/update", protectRoute, updateProfile);
 
 export default router;
