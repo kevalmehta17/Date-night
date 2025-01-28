fbaabc08e66e9cb7c1e7e846389c5ae4b91e01a2
Keval <kevalmehta885@gmail.com>
2025-01-28 20:44:25 +0530

    swipeLeft & swipeRight global state  builded

--- frontend/src/store/useMatchStore.js
+++ frontend/src/store/useMatchStore.js
@@ -7,7 +7,9 @@ export const useMatchStore = create((set) => ({
   isLoadingMyMatches: false, //this for sidebar
   isLoadingUserProfiles: false, //this for loading userprofile (at center)
   userProfiles: [],
+  swipeFeedback: null,
 
+  // this function is used to get the matches of the user
   getMyMatches: async () => {
     try {
       set({ isLoadingMyMatches: true });
@@ -20,6 +22,7 @@ export const useMatchStore = create((set) => ({
       set({ isLoadingMyMatches: false });
     }
   },
+  // this function is used to get the multiple user profiles
   getUserProfiles: async () => {
     try {
       set({ isLoadingUserProfiles: true });
@@ -30,6 +33,30 @@ export const useMatchStore = create((set) => ({
       toast.error(error.response?.data?.message || "Something went wrong");
     } finally {
       set({ isLoadingUserProfiles: false });
+    }
+  },
+
+  swipeLeft: async (user) => {
+    try {
+      await axiosInstance.post("/matches/swipe-left/" + user._id);
+      set({ swipeFeedback: "passed" });
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.response?.data?.message || "Failed to swipe Left 😢");
+    } finally {
+      setTimeout(() => set({ swipeFeedback: null }), 1500);
+    }
+  },
+
+  swipeRight: async (user) => {
+    try {
+      await axiosInstance.post("/matches/swipe-right/" + user._id);
+      set({ swipeFeedback: "liked" });
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.response?.data?.message || "Failed to swipe Right 😢");
+    } finally {
+      setTimeout(() => set({ swipeFeedback: null }), 1500);
     }
   },
 }));
