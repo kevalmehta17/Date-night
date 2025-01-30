5a58ed129d975cc67295b075dd755c4bcd2e06aa
Keval <kevalmehta885@gmail.com>
2025-01-30 19:07:10 +0530

    modified the server.js according to http socket

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { createServer } from "http";
 // Routes
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import matchRoutes from "./routes/match.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./config/db.js";
+import { initializeSocket } from "./socket/socket.server.js";
 
 dotenv.config();
 
 const app = express();
-
+const httpServer = createServer(app);
 const PORT = process.env.PORT || 5500;
+
+initializeSocket(httpServer);
 
 // Middleware
 app.use(express.json({ limit: "50mb" })); // Handling large JSON payloads
@@ -22,7 +26,7 @@ app.use(cookieParser()); // Cookie parsing middleware
 // CORS Configuration
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: process.env.CLIENT_URL, // Frontend URL
     credentials: true, // Allow cookies
   })
 );
@@ -34,7 +38,7 @@ app.use("/api/matches", matchRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Start the server and connect to the database
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
