391d70f0d04a78af225ac47ae521e74d0f3b66b9
Keval <kevalmehta885@gmail.com>
2025-02-04 02:16:59 +0530

    Deployment error

--- backend/server.js
+++ backend/server.js
@@ -42,9 +42,9 @@ app.use("/api/matches", matchRoutes);
 app.use("/api/messages", messageRoutes); // Ensure this matches the client-side URL
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
